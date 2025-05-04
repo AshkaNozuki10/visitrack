ad2f7950d7721a44c622038227da137322d360f9
Mark Kengie Aldabon <markkengiealdabon@gmail.com>
2025-05-04 11:19:46 +0800

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-return [
-    'database' => [
-        'dsn' => 'mysql:host=localhost;dbname=visitrack;charset=utf8mb4',
-        'username' => 'root',
-        'password' => 'kingsdeath10',
-    ],
-];
