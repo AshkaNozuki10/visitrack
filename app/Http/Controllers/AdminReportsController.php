e0e93f643e3d4fb53bd5a4b6d1cad06206e1dc1f
tambayNgOrtigasAvenue <markkengiealdabon@gmail.com>
2025-05-15 13:08:46 +0800

    change function name for formality

--- app/Http/Controllers/AdminReportsController.php
+++ app/Http/Controllers/AdminReportsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class AdminReportsController extends Controller
 {
-    public function index()
+    public function showReports()
     {
         return view('admin.reports');
     }
